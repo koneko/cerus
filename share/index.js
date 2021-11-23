595b096f4543b88046cbcee1e3db9814de056b74
koneko <67551503+koneko@users.noreply.github.com>
2021-11-23 21:33:49 +0100

    share test

--- share/index.js
+++ share/index.js
@@ -2,8 +2,56 @@ const params = new URLSearchParams(window.location.search)
 
 let code = params.get('code')
 let value = atob(code)
-let p = document.createElement('p')
-p.textContent = value
 console.log(value)
 
+function sharePage() {
+    let roles = JSON.parse(value.roles)
+    let members = JSON.parse(value.members)
+    content.innerHTML = `
+    <div style="display:flex; margin-bottom: 10px;">
+    <div style="width: 100px;"><b>Rank</b></div>
+    <div style="width: 150px;"><b>Username</b></div>
+    <div style="width: 60px;"><b>Points</b></div>
+    <div style="margin-left:29px;"><b>Supervisions</b></div>
+    <div style="margin-left: 35px;"><b>Strikes</b></div>
+    <div style="margin-left: 50px;"><b>Status</b></div>
+    </div>
+    `
+    for (let roleindex = 0; roleindex < roles.length; roleindex++) {
+        let rolename = roles[roleindex].name
+        var filter = members.filter((member) => { 
+            return member.role == rolename
+        });
+        filter.forEach(member => {
+            let role = roles.find(v => v.name == member.role)
+            let div = document.createElement('div')
+            let memberstatus = null
+            if(member.status == "failed") {
+                memberstatus = statusList.failed
+            } else if(member.status == "passed") {
+                memberstatus = statusList.passed
+            } else if(member.status == "inactive") {
+                memberstatus = statusList.inactive
+            } else {
+                memberstatus = statusList.immune
+            }
+            div.style.display = "flex"
+            div.style.marginBottom = "19.5px"
+            div.style.marginTop = "19.5px"
+            div.removeAttribute("tabIndex");
+            div.innerHTML = `
+            <div style="width: 100px;"><span style="color:${role.color};">${role.name}</span></div>
+            <div style="width: 150px;"><span>${member.name}</span></div>
+            <div style="width: 70x; margin-left: 10px;"><span style="margin-right: 10px;"></span><b>${member.points}</b></div>
+            <div style="margin-left:90px;"><span style="margin-right: 10px;"></span><b>${member.supervisions}</b></div>
+            <div style="margin-left:85px;"><span style="margin-right: 10px;"></span><b>${member.strikes}</b></div>
+            <div style="margin-left:90px;"><span>${memberstatus}</span></div>
+            
+            `
+            content.appendChild(div)
+        });
+    }
 
+}
+
+sharePage()
